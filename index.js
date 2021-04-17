429c88413916e6c64adaef112a7350732845bac4
Nate Griggs <nateghsc09@gmail.com>
2021-04-17 15:26:58 -0500

    lost

--- index.js
+++ index.js
@@ -1,62 +1,135 @@
-const Employee = require("../lib/Employee");
-test("Can instantiate Employee instance", () => {
-  const e = new Employee();
-  expect(typeof(e)).toBe("object");
-});
-test("Can set name via constructor arguments", () => {
-  const name = "Alice";
-  const e = new Employee(name);
-  expect(e.name).toBe(name);
-});
-test("Can set id via constructor argument", () => {
-  const testValue = 100;
-  const e = new Employee("Foo", testValue);
-  expect(e.id).toBe(testValue);
-});
-test("Can set email via constructor argument", () => {
-  const testValue = "test@test.com";
-  const e = new Employee("Foo", 1, testValue);
-  expect(e.email).toBe(testValue);
-});
-test("Can get name via getName()", () => {
-  const testValue = "Alice";
-  const e = new Employee(testValue);
-  expect(e.getName()).toBe(testValue);
-});
-test("Can get id via getId()", () => {
-  const testValue = 100;
-  const e = new Employee("Foo", testValue);
-  expect(e.getId()).toBe(testValue);
-});
-test("Can get email via getEmail()", () => {
-  const testValue = "test@test.com";
-  const e = new Employee("Foo", 1, testValue);
-  expect(e.getEmail()).toBe(testValue);
-});
-test("getRole() should return \"Employee\"", () => {
-  const testValue = "Employee";
-  const e = new Employee("Alice", 1, "test@test.com");
-  expect(e.getRole()).toBe(testValue);
+const inquirer = require("inquirer");
 
-  
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const html = require("./templates/htmltemp");
 
 
 
-  const Manager = require("../lib/Manager");
-  
-const Employee = require("../lib/Employee");
-test("Can set office number via constructor argument", () => {
-  const testValue = 100;
-  const e = new Manager("Foo", 1, "test@test.com", testValue);
-  expect(e.officeNumber).toBe(testValue);
-});
-test("getRole() should return \"Manager\"", () => {
-  const testValue = "Manager";
-  const e = new Manager("Foo", 1, "test@test.com", 100);
-  expect(e.getRole()).toBe(testValue);
-});
-test("Can get office number via getOffice()", () => {
-  const testValue = 100;
-  const e = new Manager("Foo", 1, "test@test.com", testValue);
-  expect(e.getOfficeNumber()).toBe(testValue);
-});
+let teamArray = [];
+let teamstr = ``;
+
+//this calls all the functions in one in order
+async function main() {
+     try {
+          await prompt()
+          // for i to teamArray.length  => 
+
+          for (let i = 0; i < teamArray.length; i++) {
+               //template literal=``
+               teamstr = teamstr + html.generateCard(teamArray[i]);
+          }
+
+          let finalHTML = html.generateHTML(teamstr)
+
+          console.log(teamstr)
+
+          //call generate function to generate the html template literal
+
+          //write file 
+          writeFileAsync("./output/index.html", finalHTML)
+
+
+     } catch (err) {
+          return console.log(err);
+     }
+
+};
+
+async function prompt() {
+     let responseDone = "";
+     // prompt to collect input and use do while atleast one and do it number of times depending on the while condition
+     do {
+          try {
+               response = await inquirer.prompt([
+
+                    {
+                         type: "input",
+                         name: "name",
+                         message: "What is the employee's name?: "
+                    },
+                    {
+                         type: "input",
+                         name: "id",
+                         message: "Enter the employee's ID: "
+                    },
+                    {
+                         type: "input",
+                         name: "email",
+                         message: "Enter the employee's email address: "
+                    },
+                    {
+                         type: "list",
+                         name: "role",
+                         message: "What what is the employee's role:",
+                         choices: [
+                              "Engineer",
+                              "Intern",
+                              "Manager"
+                         ]
+                    }
+               ]);
+
+               let response2 = ""
+               // if else statement
+
+               if (response.role === "Engineer") {
+                    response2 = await inquirer.prompt([{
+                         type: "input",
+                         name: "x",
+                         message: "What is the employee's github username?:",
+                    }, ]);
+                    //store the object and push
+                    const engineer = new Engineer(response.name, response.id, response.email, response2.x);
+                    teamArray.push(engineer);
+               } else if (response.role === "Intern") {
+                    response2 = await inquirer.prompt([{
+                         type: "input",
+
+                         //the x is to only store into the team array
+                         name: "x",
+                         message: "What school is the employee attending?:",
+                    }, ]);
+                    //store the object and push
+                    const intern = new Intern(response.name, response.id, response.email, response2.x);
+                    teamArray.push(intern);
+               } else if (response.role === "Manager") {
+                    response2 = await inquirer.prompt([{
+                         type: "input",
+                         name: "x",
+                         message: "What is the employee's office number?:",
+                    }, ]);
+                    //store the object and push
+                    const manager = new Manager(response.name, response.id, response.email, response2.x);
+                    teamArray.push(manager);
+               }
+          } catch (err) {
+               return console.log(err);
+          }
+          console.log(teamArray)
+          //need to prompt do you want to continue
+
+          responseDone = await inquirer.prompt([{
+               type: "list",
+               name: "finish",
+               message: "Do you want to continue?: ",
+               choices: [
+                    "Yes",
+                    "No"
+               ]
+          }, ]);
+
+          // console.log(responseDone.choices);
+          //the while parameter is saying continue running the code if the user selects "yes"
+     } while (responseDone.finish === "Yes");
+}
+
+
+
+
+
+
+
+//call function to run application on the server
+main();
